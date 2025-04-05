fc6463b72a49336ecc8ee0698c8a5ead870c5ea3
Dimma Don't <93484965+DimmaDont@users.noreply.github.com>
2025-04-05 17:06:01 -0400

    fix: lib import when using pycryptodome

--- src/crypt_sindresorhus_conf/__init__.py
+++ src/crypt_sindresorhus_conf/__init__.py
@@ -1,4 +1,7 @@
-from .crypt_sindresorhus_conf import CryptSindresorhusConf
+try:
+    from .crypt_sindresorhus_conf import CryptSindresorhusConf
+except ImportError:
+    from .crypt_sindresorhus_conf_pycryptodome import CryptSindresorhusConf  # type: ignore
 
 
 __all__ = ["CryptSindresorhusConf"]
